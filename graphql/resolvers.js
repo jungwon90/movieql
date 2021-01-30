1019cec09d67dfe1365eb22e4471973c2167667c
Jungwon Goda <koag132@gmail.com>
2021-01-29 17:28:56 -0800

    Defined Graphql Query and resolvers

---
+++ graphql/resolvers.js
@@ -0,0 +1,12 @@
+
+//resolves the Query
+//basically programming how we are going to send the response to the user
+
+const resolvers = {
+    //write a resolvers for Query
+    Query: {
+        name: () => `Jungwon`,
+    },
+};
+
+export default resolvers;
--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 import { GraphQLServer } from 'graphql-yoga';
+import resolvers from './graphql/resolvers';
 
-const typeDefs = `
-    type Query {
-        hello(name: String): String!
-    }
-`
+//need to define a schema(schema: description about what r u gonna get, what r u gonna send)
 
-const resolvers = {
-    Query: {
-        hello: (_, {name}) => `Hello ${name || 'World'}`,
-    },
-}
 
-const server = new GraphQLServer({typeDefs, resolvers})
+//what the GraphQLServer wants us to do is 
+//that which queries do we have and which mutation do we have
+//so the graphql can understand
+const server = new GraphQLServer({ 
+    typeDefs: "graphql/schema.graphql", 
+    resolvers 
+});
+
 server.start(() => console.log('Graphql Server is running on localhost:4000'));
