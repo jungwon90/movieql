3cef5045e4fbabdf709dbbc8f340775c3444b857
Jungwon Goda <koag132@gmail.com>
2021-02-01 16:36:54 -0800

    created queries with arguments

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# movieql
+### movieql
 Movie API - Graphql
+
+### Technologies required 
+Node.js, Javascript, GraphQL
+Libraries/Framework: Graphql-yoga
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,11 +1,37 @@
 
 //resolves the Query
 //basically programming how we are going to send the response to the user
+const people = [
+    {   
+        id: 0,
+        name: "Jungwon",
+        age: 30,
+        gender: "female"
+    },
+    {   
+        id: 1,
+        name: "Yusai",
+        age: 43,
+        gender: "male"
+    },
+    {   
+        id: 2,
+        name: "Danbi",
+        age: 28,
+        gender: "female"
+    }
+];
+
+const getById = id => {
+    const filterdPeople = people.filter(person => person.id === id);
+    return filterdPeople[0];
+}
 
 const resolvers = {
     //write a resolvers for Query
     Query: {
-        name: () => `Jungwon`,
+        people: () => people,
+        person: (_, {id}) => getById(id)
     },
 };
 
